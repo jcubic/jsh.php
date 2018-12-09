475b520438d798d92f701fe1c9c873c9579228b0
Jakub Jankiewicz <jcubic@onet.pl>
2018-12-09 20:09:11 +0100

    mysql command #1 + various fixes

--- jsh.php
+++ jsh.php
@@ -9,7 +9,7 @@
  */
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
 
 // leave blank or delete if don't want password protection
 $config = array(
@@ -32,12 +32,66 @@ class App {
         return implode("\n", $result);
     }
     // ------------------------------------------------------------------------
+    public function uniq_id() {
+        return md5(array_sum(explode(" ", microtime())) * mktime());
+    }
+    // ------------------------------------------------------------------------
     public function system($code) {
         ob_start();
         system($code);
         $result = ob_get_contents();
         ob_end_clean();
         return $result;
+    }
+    // ------------------------------------------------------------------------
+    public function mysql_close($id) {
+        if (isset($_SESSION['mysql']) && isset($_SESSION['mysql'][$id])) {
+            unset($_SESSION['mysql'][$id]);
+            return true;
+        }
+        return false;
+    }
+    // ------------------------------------------------------------------------
+    public function mysql_connect($username, $password, $db_name, $host = 'localhost') {
+        $db = $this->_mysql_connect($username, $password, $db_name, $host);
+        if (!isset($_SESSION['mysql'])) {
+            $_SESSION['mysql'] = array();
+        }
+        $resource_id = $this->uniq_id();
+        $_SESSION['mysql'][$resource_id] = array(
+            'username' => $username,
+            'password' => $password,
+            'host' => $host,
+            'db' => $db_name
+        );
+        return $resource_id;
+    }
+    // ------------------------------------------------------------------------
+    public function mysql_query($resource_id, $query) {
+        if (!isset($_SESSION['mysql'])) {
+            throw new Exception("Not mysql sessions found");
+        }
+        if (!isset($_SESSION['mysql'][$resource_id])) {
+            throw new Exception("Invalid resource id");
+        }
+        $data = $_SESSION['mysql'][$resource_id];
+        $db = $this->_mysql_connect($data['username'], $data['password'], $data['db'], $data['host']);
+        $res = $db->query($query);
+        if ($res) {
+            if (preg_match("/^\s*INSERT|UPDATE|DELETE|ALTER|CREATE|DROP/i", $query)) {
+                return $res->rowCount();
+            } else {
+                    return $res->fetchAll(PDO::FETCH_ASSOC);
+            }
+        } else {
+            throw new Exception("Query Error");
+        }
+    }
+    // ------------------------------------------------------------------------
+    private function _mysql_connect($username, $password, $db, $host = 'localhost') {
+      $opts = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+      $db = new PDO("mysql:host={$host};dbname={$db};charset=utf8mb4", $username, $password, $opts);
+      return $db;
     }
     // ------------------------------------------------------------------------
     private function unbuffer($command, $shell_fn) {
@@ -52,9 +106,7 @@ class App {
             }
         }
     }
-    public function utf8($string) {
-        mb_detect_encoding($string);
-    }
+    // ------------------------------------------------------------------------
     public function command($command, $path, $shell_fn) {
         if (!method_exists($this, $shell_fn)) {
             throw new Exception("Invalid shell '$shell_fn'");
@@ -112,6 +164,7 @@ class App {
             throw new Exception("Internal error, shell function give no result");
         }
     }
+    // ------------------------------------------------------------------------
     public function shell($code) {
         $shells = array('system', 'exec', 'shell_exec');
         foreach ($shells as $shell) {
@@ -128,6 +181,7 @@ function token() {
     return sha1($time) . substr(md5($time), 4);
 }
 function password_set() {
+    global $config;
     return isset($config['password']) && $config['password'] != '';
 }
 session_start();
@@ -154,12 +208,47 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
             } catch(Exception $e) {
                 echo json_encode(array("error" => $e->getMessage()));
             }
+        } else if ($_POST['action'] == 'rpc' && isset($_POST['method'])) {
+            $class = get_class($app);
+            $methods = get_class_methods($class);
+            if ($_POST['method'] == 'system.describe') {
+                echo json_encode(array('result' => array_values(array_filter(array_map(function($name) use ($class) {
+                  $method = new ReflectionMethod($class, $name);
+                  if ($method->isPublic() && !$method->isStatic() &&
+                      !$method->isConstructor() && !$method->isDestructor()) {
+                    return array('name' => $name, 'params' => $method->getNumberOfRequiredParameters());
+                  }
+                }, $methods)))));
+            } else {
+                try {
+                    $params = isset($_POST['params']) ? $_POST['params'] : array();
+                    if (!in_array($_POST['method'], $methods) && !in_array("__call", $methods)) {
+                        echo json_encode(array("error" => "Method {$_POST['method']} not found"));
+                    } else if (in_array("__call", $methods) && !in_array($method, $methods)) {
+                        $result = call_user_func_array(array($obj, $_POST['method']), $params);
+                        echo json_encode(array('result' => $result));
+                    } else {
+                        $method_object = new ReflectionMethod($class, $_POST['method']);
+                        $num_got = count($params);
+                        $num_expect = $method_object->getNumberOfRequiredParameters();
+                        if ($num_got != $num_expect) {
+                            $msg = "Wrong number of parameters in `{$_POST['method']}' method. Got " .
+                                   "$num_got expect $num_expect";
+                            echo json_encode(array("error" => $msg));
+                        } else {
+                            $result = call_user_func_array(array($app, $_POST['method']), $params);
+                            echo json_encode(array('result' => $result));
+                        }
+                    }
+                } catch (Exception $e) {
+                    echo json_encode(array('error' => $e->getMessage()));
+                }
+            }
         }
+    } else {
+        echo json_encode(array('error' => 'Invalid Token'));
     }
-    exit();
-}
-
-
+} else {
 ?>
 <!DOCTYPE html>
 <html>
@@ -167,9 +256,10 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
 <title>PHP Shell</title>
 <link rel="shortcut icon" href="https://raw.githubusercontent.com/jcubic/jquery.terminal-www/master/favicon.ico"/>
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="https://cdn.rawgit.com/jcubic/jquery.terminal/master/js/jquery.terminal.min.js"></script>
-<script src="https://cdn.rawgit.com/jcubic/jquery.terminal/master/js/unix_formatting.js"></script>
-<link href="https://cdn.rawgit.com/jcubic/jquery.terminal/master/css/jquery.terminal.min.css"
+<script src="https://unpkg.com/jquery.terminal/js/jquery.terminal.min.js"></script>
+<script src="https://unpkg.com/jquery.terminal/js/unix_formatting.js"></script>
+<script src="https://unpkg.com/jquery.terminal/js/ascii_table.js"></script>
+<link href="https://unpkg.com/jquery.terminal/css/jquery.terminal.min.css"
       rel="stylesheet"/>
 <style>
 body {
@@ -179,66 +269,214 @@ body {
 </style>
 <body>
 <script>
-$(function() {
-  var config = <?= json_encode(array_merge($config, array('password' => isset($config['password']) && $config['password'] != ''))) ?>;
-  var cwd = config.root;
-  function init(term, token) {
-    term.set_mask(false).push(function(cmd) {
-      var payload = {cmd: cmd, action: 'shell', path: cwd};
-      if (config.password) {
-        if (token) {
-          payload.token = token;
-        } else {
-          term.error('no token');
-          return;
-        }
-      }
-      term.pause();
-      $.post('', payload).then(function(data) {
-        if (data.error) {
-          term.error(data.error);
-        } else {
-          term.echo(data.output.trim());
-          cwd = data.cwd;
-        }
-        term.resume();
-      });
-    }, {
-      prompt: function(set) {
-        set(cwd + '# ');
-      },
-      onExit: function() {
-        $.Storage.remove('token');
-      }
-    });
-  }
-  $('body').terminal(function(password, term) {
-    term.pause();
-    $.post('', {action: 'login', password: password}).then(function(data) {
-      if (data.error) {
-        term.error(data.error);
-      } else {
-        if (config.storage) {
-          $.Storage.set('token', data.result);
-        }
-        init(term, data.result);
-      }
-      term.resume();
-    });
-  }, {
-    greetings: 'jsh shell',
-    prompt: 'password: ',
-    onInit: function(term) {
-      term.set_mask(true);
-      if (config.password) {
-        if (config.storage && $.Storage.get('token')) {
-          init(term, $.Storage.get('token'));
-        }
-      } else {
-        init(term);
-      }
-    }
-  });
-});
+ jQuery(function($) {
+     var config = <?= json_encode(array_merge($config, array('password' => isset($config['password']) && $config['password'] != ''))) ?>;
+     var cwd = config.root;
+     // --------------------------------------------------------------------------------------------
+     function init(term, token) {
+         function sql_result(result) {
+             switch ($.type(result)) {
+                 case 'array':
+                     if (result.length) {
+                         var keys = Object.keys(result[0]);
+                         result = [keys].concat(result.map(function(row) {
+                             if (row instanceof Array) {
+                                 return row.map(function(item) {
+                                     return String(item);
+                                 });
+                             } else {
+                                 return Object.keys(row).map(function(key) {
+                                     return String(row[key]);
+                                 });
+                             }
+                         }));
+                         term.echo(ascii_table(result, true), {formatters: false});
+                     }
+                     break;
+                 case 'number':
+                     term.echo('Query OK, ' + result + ' row' + (result != 1 ? 's' : '') + ' affected');
+                 default:
+                     // should not happen
+                     term.echo(result);
+             }
+             term.resume();
+         }
+         // ----------------------------------------------------------------------------------------
+         function mysql(user, password, db, host) {
+             var params = [user, password, db];
+             if (typeof host !== 'undefiend') {
+                 params.push(host);
+             }
+             term.pause();
+             // clear old mysql connection if closed the browser tab while in mysql
+             if (config.storage) {
+                 var id = $.Storage.get('mysql_connection');
+                 if (id) {
+                     $.Storage.remove('mysql_connection');
+                     rpc('mysql_close', [id]);
+                 }
+             }
+             rpc('mysql_connect', params).then(function(id) {
+                 if (config.storage) {
+                     $.Storage.set('mysql_connection', id);
+                 }
+                 term.resume().push(function(query) {
+                     term.pause();
+                     rpc('mysql_query', [id, query]).then(sql_result);
+                 }, {
+                     name: 'mysql',
+                     prompt: 'mysql> ',
+                     onExit: function() {
+                         if (config.storage) {
+                             $.Storage.remove('mysql_connection');
+                         }
+                         rpc('mysql_close', [id]);
+                     }
+                 });
+             });
+         }
+         // ----------------------------------------------------------------------------------------
+         function rpc(method, params) {
+             if (typeof params === 'undefined') {
+                 params = [];
+             }
+             var payload = {action: 'rpc', method: method, params: params};
+             if (config.password) {
+                 if (token) {
+                     payload.token = token;
+                 } else {
+                     term.error('no token');
+                     return;
+                 }
+             }
+             var defer = $.Deferred();
+             $.post('', payload, function(data) {
+                 try {
+                     data = JSON.parse(data.replace(/\\r/g, ''));
+                     if (data.error) {
+                         term.error(data.error).resume();
+                     } else {
+                         defer.resolve(data.result);
+                     }
+                 } catch (e) {
+                     term.error('JSON parse error').resume();
+                 }
+             }, 'text');
+             return defer.promise();
+         }
+         // ----------------------------------------------------------------------------------------
+         term.set_mask(false).push(function(command) {
+             var cmd = $.terminal.parse_command(command);
+             if (cmd.name == 'mysql') {
+                 var password;
+                 cmd.args = cmd.args.filter(function(arg) {
+                     var m = arg.match(/^-p(.+)/);
+                     if (m) {
+                         password = m[1];
+                         return false;
+                     }
+                     return true;
+                 });
+                 var options = $.terminal.parse_options(cmd.args, {boolean: 'p'});
+                 var host;
+                 if (options.h) {
+                     host = options.h;
+                 }
+                 if (options.u && options._.length > 0) {
+                     if (!password) {
+                         term.set_mask(true).read('password: ').then(function(pass) {
+                             password = pass;
+                             term.set_mask(false);
+                             mysql(options.u, password, options._[0], host);
+                         });
+                     } else {
+                         mysql(options.u, password, options._[0], host);
+                     }
+                 } else {
+                     term.echo('usage: mysql -u <USER> -p<PASSWORD> -h <HOST> <DB NAME>');
+                 }
+             } else if (cmd.name == 'rpc') {
+                 term.pause();
+                 rpc('system.describe').then(function(data) {
+                     if (data.error) {
+                         term.error(data.error);
+                     } else {
+                         term.push(function(command) {
+                             cmd = $.terminal.parse_command(command);
+                             rpc(cmd.name, cmd.args).then(function(data) {
+                                 if (data.error) {
+                                     term.error(data.error);
+                                 } else {
+                                     term.echo(data.result);
+                                 }
+                             });
+                         }, {
+                             completion: data.result.map(function(method) { return method.name; }),
+                             name: 'rpc',
+                             prompt: 'rpc> '
+                         });
+                     }
+                     term.resume();
+                 });
+             } else {
+                 var payload = {cmd: command, action: 'shell', path: cwd};
+                 if (config.password) {
+                     if (token) {
+                         payload.token = token;
+                     } else {
+                         term.error('no token');
+                         return;
+                     }
+                 }
+                 term.pause();
+                 $.post('', payload).then(function(data) {
+                     if (data.error) {
+                         term.error(data.error);
+                     } else {
+                         term.echo(data.output.trim());
+                         cwd = data.cwd;
+                     }
+                     term.resume();
+                 });
+             }
+         }, {
+             prompt: function(set) {
+                 set(cwd + '# ');
+             },
+             onExit: function() {
+                 $.Storage.remove('token');
+             }
+         });
+     }
+     // --------------------------------------------------------------------------------------------
+     $('body').terminal(function(password, term) {
+         term.pause();
+         $.post('', {action: 'login', password: password}).then(function(data) {
+             if (data.error) {
+                 term.error(data.error);
+             } else {
+                 if (config.storage) {
+                     $.Storage.set('token', data.result);
+                 }
+                 init(term, data.result);
+             }
+             term.resume();
+         });
+     }, {
+         greetings: 'jsh shell',
+         prompt: 'password: ',
+         onInit: function(term) {
+             term.set_mask(true);
+             if (config.password) {
+                 if (config.storage && $.Storage.get('token')) {
+                     init(term, $.Storage.get('token'));
+                 }
+             } else {
+                 init(term);
+             }
+         }
+     });
+ });
 </script>
 </body>
+<?php } ?>
